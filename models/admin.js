f4f6531e6cbb2f1f55c0e4ba426ff255d38cf45a
Rajeev Shukla <36722623+illusionrs@users.noreply.github.com>
2020-09-25 11:12:34 +0530

    admin added

---
+++ models/admin.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  }
+});
+
+const Admin = mongoose.model("User", UserSchema);
+module.exports = Admin;
